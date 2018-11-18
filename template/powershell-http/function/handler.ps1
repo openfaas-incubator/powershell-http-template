76f77e5e0e113113bf57df2dbc385b21acc09f36
Burton Rheutan <rheutan7@gmail.com>
2018-11-18 16:28:47 -0600

    Add powershell-http template
    This is an initial powershell-http template
    Create an HTTP server in powershell and call the
    function/handler.ps1 script with the incoming request
    body
    
    Errors are returned with a 500 status code and the
    exception message
    
    Signed-off-by: Burton Rheutan <rheutan7@gmail.com>

---
+++ template/powershell-http/function/handler.ps1
@@ -0,0 +1,6 @@
+# Rename input for better readability
+$funcInput = $args
+$output = "Hello! Your input was: " + $funcInput
+
+# Must provide an output variable for the main process to return
+$output
