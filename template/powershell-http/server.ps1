771c9a1380879c94cf7f5812eebe6f49efe1aac4
Burton Rheutan <rheutan7@gmail.com>
2018-12-26 11:37:33 -0600

    Update to use context and response classes
    Adding new class objects to be similar to other http templates
    
    These new classes allow the function to access the request
    and other propoerties as well as being able to modify the
    response as needed with some sane defaults
    
    Signed-off-by: Burton Rheutan <rheutan7@gmail.com>

--- template/powershell-http/server.ps1
+++ template/powershell-http/server.ps1
@@ -1,5 +1,20 @@
 Add-Type -AssemblyName System.Web
 
+Class FunctionContext{
+    [string] $Method;
+    [string] $Request;
+    [string] $Body;
+    [System.Net.WebHeaderCollection] $Headers;
+    [System.Collections.Specialized.NameValueCollection] $Query;
+    [string] $Path;
+}
+
+Class FunctionResponse{
+    [string] $Body;
+    [System.Net.WebHeaderCollection] $Headers;
+    [int] $Status;
+}
+
 $listener = New-Object System.Net.HttpListener
 $listener.Prefixes.Add("http://*:8081/")
 $listener.Start()
@@ -14,19 +29,29 @@ do {
 
     $reader = [System.IO.StreamReader]::new($reqStream, $reqEncoding)
 
-    $reqBody = $reader.ReadToEnd()
+    $fnContext = [FunctionContext]::new()
+    $fnContext.Body = $reader.ReadToEnd()
+    $fnContext.Headers = $context.Request.Headers
+    $fnContext.Method = $context.Request.HttpMethod
+    $fnContext.Query = $context.Request.QueryString
+    $fnContext.Path = $context.Request.Url.LocalPath
+    $fnContext.Request = $context.Request
 
+    $fnResponse = [FunctionResponse]::new()
+    $fnResponse.Headers = [System.Net.WebHeaderCollection]::new()
     try {
-        $funcResult = . .\function\handler.ps1 $reqBody
+        . .\function\handler.ps1 -fnContext $fnContext -fnResponse $fnResponse
 
-        $Content = [System.Text.Encoding]::UTF8.GetBytes($funcResult)
-        $response.StatusCode = 200
+        $Content = [System.Text.Encoding]::UTF8.GetBytes($fnResponse.Body)
+        $response.StatusCode = $(If ($fnResponse.Status) {$fnResponse.Status} Else {200}) #default to 200 response if not set
     }
     catch {
-        $Content = [System.Text.Encoding]::UTF8.GetBytes("$_.Exception.Message")
-        $response.StatusCode = 500
+        $responseBody = $(If ($_.Exception.Message) {$_.Exception.Message} Else {$fnResponse.Body})
+        $Content = [System.Text.Encoding]::UTF8.GetBytes($responseBody)
+        $response.StatusCode = $(If ($fnResponse.Status) {$fnResponse.Status} Else {500}) #default to 500 response for exceptions
     }
 
+    $response.Headers = $fnResponse.Headers
     $response.ContentLength64 = $Content.Length
     $response.OutputStream.Write($Content, 0, $Content.Length)
     $response.Close()
